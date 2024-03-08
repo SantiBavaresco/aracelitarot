5b6f35286522024ae781ec77be70113c93c9eea0
Santiago Bavaresco <103169408+SantiBavaresco@users.noreply.github.com>
2024-03-08 15:58:55 -0300

    Actualizar layout3.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
 			<head >
 <meta property="og:type" content="website"/>
 <meta property="og:image" content="../public/tAraceli2.png"/>
-<meta property="og:image:width" content="1280"/>
-<meta property="og:image:height" content="720"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="690"/>
 <meta property="og:image:ratio" content="16:9"/>
 			</head>
 			<body
